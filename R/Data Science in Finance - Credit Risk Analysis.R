aca33320211a16521149602535016745bad8d21c
Fauzan Abiyyu Aziz <106464386+Fauzanaziz03@users.noreply.github.com>
2023-01-31 17:23:18 +0700

    Add files via upload

---
+++ R/Data Science in Finance - Credit Risk Analysis.R
@@ -0,0 +1,466 @@
+#Credit risk adalah resiko yang harus ditanggung oleh seorang individu atau lembaga ketika memberikan pinjaman - biasanya dalam bentuk uang - ke individu atau pihak lain.
+
+#Resiko ini berupa tidak bisa dibayarkannya pokok dan bunga pinjaman, sehingga mengakibatkan kerugian.
+#Credit score dan risk rating adalah dua penilaian yang dilakukan meminimalkan resiko dari pihak pemberi kredit. Karena berdasarkan kedua model penilaian tersebut, akan diputuskan apakah aplikasi peminjaman seseorang disetujui atau ditolak.
+
+#1. Decision Tree
+#Decision tree adalah struktur model untuk pengambilan keputusan dimana kita mengikuti alur dari suatu titik awal (yang disebut root node), kondisi-kondisi berikutnya, sampai mencapai kesimpulan.
+
+    #Contoh Pemodelan Decision Tree dengan Machine Learnig
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+
+#Menggunakan C5.0
+drop_columns <- c("kpr_aktif", "pendapatan_setahun_juta", "risk_rating", "rata_rata_overdue")
+datafeed <- dataCreditRating[ , !(names(dataCreditRating) %in% drop_columns)]
+modelKu <- C5.0(datafeed, as.factor(dataCreditRating$risk_rating))
+summary(modelKu)
+
+
+#2. Algoritma C5.0
+
+    #Data Preparation untuk Class Variable
+
+library("openxlsx")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+str(dataCreditRating)
+
+#Melakukan konversi kolom risk_rating menjadi factor
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating)
+
+#Melihat struktur setelah konversi
+str(dataCreditRating)
+
+
+    #Data Preparation untuk Input Variable
+
+library("openxlsx")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Merubah tipe data class variable sebagai factor 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+str(dataCreditRating)
+
+#Menghilangkan beberapa variable input dari dataset 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+str(datafeed)
+
+
+    #Mempersiapkan Training Set dan Testing Set
+
+library("openxlsx")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan porsi index acak untuk training dan testing set
+set.seed(100)
+indeks_training_set <- sample(900,800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+str(input_training_set)
+str(class_training_set)
+str(input_testing_set)
+
+#Fungsi dari perintah set.seed ialah untuk melakukan seeding sehingga nilai random selalu sama di setiap versi R yang digunakan.
+
+
+    #Menghasilkan Model dengan Fungsi C5.0
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan dan menampilkan summary model
+risk_rating_model <- C5.0(input_training_set, class_training_set)
+summary(risk_rating_model)
+
+#Dataset yang diperlukan untuk menghasilkan model C5.0 ialah training set untuk input variables dan class variable
+
+
+    #Menghasilkan Plot dari Model C5.0
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan dan menampilkan summary model
+risk_rating_model <- C5.0(input_training_set, class_training_set)
+plot(risk_rating_model)
+
+
+#3. Memahami Output dari Fungsi C5.0
+
+    #Label dari Class
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+summary(risk_rating_model)
+
+
+    #Jumlah Data yang Digunakan
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan","kpr_aktif")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 780)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+summary(risk_rating_model)
+
+
+    #Menganalisa Label Class Variable
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating[dataCreditRating$risk_rating == "1"] <- "satu"
+dataCreditRating$risk_rating[dataCreditRating$risk_rating == "2"] <- "dua"
+dataCreditRating$risk_rating[dataCreditRating$risk_rating == "3"] <- "tiga"
+dataCreditRating$risk_rating[dataCreditRating$risk_rating == "4"] <- "empat"
+dataCreditRating$risk_rating[dataCreditRating$risk_rating == "5"] <- "lima"
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+summary(risk_rating_model)
+
+#Dengan tingkat error 22.5 persen, model ini harusnya cukup layak digunakan. Dari confusion matrix juga terlihat banyak sekali yang diprediksi dengan benar, terutama untuk nilai risk_rating 4 dan 5.
+
+
+#4. Evaluasi Model
+
+    #Menggunakan Fungsi Predict
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+
+#menggunakan model untuk prediksi testing set
+predict(risk_rating_model, input_testing_set)
+
+
+    #Menggabungkan Hasil Prediksi
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+
+#menyimpan hasil prediksi testing set ke dalam kolom hasil_prediksi
+input_testing_set$risk_rating <- dataCreditRating[-indeks_training_set,]$risk_rating
+input_testing_set$hasil_prediksi <- predict(risk_rating_model, input_testing_set)
+print(input_testing_set)
+
+
+    #Membuat Table Confusion Matrix
+
+library("openxlsx")
+library("C50")
+library("reshape2")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+
+#menyimpan risk_rating dari data awal dan hasil prediksi testing set ke dalam kolom hasil_prediksi
+input_testing_set$risk_rating <- dataCreditRating[-indeks_training_set,]$risk_rating
+input_testing_set$hasil_prediksi <- predict(risk_rating_model, input_testing_set)
+
+#membuat confusion matrix
+dcast(hasil_prediksi ~ risk_rating, data=input_testing_set)
+
+
+    #Jumlah Data dengan Prediksi Benar
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+
+#menyimpan risk_rating dari data awal dan hasil prediksi testing set ke dalam kolom hasil_prediksi
+input_testing_set$risk_rating <- dataCreditRating[-indeks_training_set,]$risk_rating
+input_testing_set$hasil_prediksi <- predict(risk_rating_model, input_testing_set)
+
+#Menghitung jumlah prediksi yang benar
+nrow(input_testing_set[input_testing_set$risk_rating==input_testing_set$hasil_prediksi,])
+
+
+    #Jumlah Data dengan Prediksi Salah
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
+
+#menyimpan risk_rating dari data awal dan hasil prediksi testing set ke dalam kolom hasil_prediksi
+input_testing_set$risk_rating <- dataCreditRating[-indeks_training_set,]$risk_rating
+input_testing_set$hasil_prediksi <- predict(risk_rating_model, input_testing_set)
+
+#Menghitung jumlah prediksi yang salah
+nrow(input_testing_set[input_testing_set$risk_rating!=input_testing_set$hasil_prediksi,])
+
+
+#5. Menggunakan Model untuk Prediksi
+
+    #Mempersiapkan Data Pengajuan Baru
+
+#Membuat data frame aplikasi baru
+aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 12)
+print(aplikasi_baru)
+
+
+    #Melakukan Prediksi terhadap Data Pengajuan Baru
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan dan menampilkan summary model
+risk_rating_model <- C5.0(input_training_set, class_training_set)
+
+#Membuat data frame aplikasi baru
+aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 12)
+
+#melakukan prediksi
+predict(risk_rating_model, aplikasi_baru)
+
+
+    #Merubah Durasi Pinjaman
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan dan menampilkan summary model
+risk_rating_model <- C5.0(input_training_set, class_training_set)
+
+#Membuat data frame aplikasi baru
+aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 64)
+
+#melakukan prediksi
+predict(risk_rating_model, aplikasi_baru)
+
